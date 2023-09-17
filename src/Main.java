a5b4b72c33a1efcd3413a93a2f5cb408d7e88a69
Ekanto3321 <araddhaekanto@gmail.com>
2023-09-17 22:09:51 +0600

    I changed something

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Wall.mapToUse="test.ppm";
+        Wall.mapToUse= "map.ppm";
         new UserInterface();
         new Core().loop();
 
